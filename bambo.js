4f6806e4bcf966fca069c6c575ed7b3ef68e37f4
Abrams11IsBack <abrams11isback@gmail.com>
2023-05-09 16:34:57 +0200

    h

--- bambo.js
+++ bambo.js
@@ -6,6 +6,7 @@ const koniecSwiata = document.querySelector("#dron");
 aktywator.addEventListener('click', () => {
     //tutaj sie zaczyna prawdziwy męski kod a nie jakies kłeryselektory i strzałki
     while (1 == 1) {
-        koniecSwiata.appendChild(document.createElement("<h1>DUDU REALISTA</h1>"));
+        koniecSwiata.appendChild(document.createElement("p"));
+        koniecSwiata.appendChild(document.createTextNode("DUDU REALISTA"));
     }
 });

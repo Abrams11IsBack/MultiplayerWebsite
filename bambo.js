5f903914912f9eb3b3ebb6ea0d10f939bf7393b2
Abrams11IsBack <abrams11isback@gmail.com>
2023-05-09 16:28:15 +0200

    added nuclear bomb test1

---
+++ bambo.js
@@ -0,0 +1,9 @@
+const aktywator = document.querySelector("button");
+const koniecSwiata = document.querySelector("#dron");
+
+aktywator.addEventListener('click', () => {
+    //tutaj sie zaczyna prawdziwy męski kod a nie jakies kłeryselektory i strzałki
+    while (1 == 1) {
+        koniecSwiata.appendChild(document.createElement("<h1>DUDU REALISTA</h1>"));
+    }
+});

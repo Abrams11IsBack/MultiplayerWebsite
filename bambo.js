767b7156008cf75f8fb4d0cc5b4a100dba9fed10
Abrams11IsBack <abrams11isback@gmail.com>
2023-05-09 16:49:12 +0200

    dodano nowy sex

--- bambo.js
+++ bambo.js
@@ -1,12 +1,18 @@
 alert("gratulacje użytkowniku");
 
-const aktywator = document.querySelector("button");
+const aktywator = document.querySelector("#a");
+const destruktator = document.querySelector("#b");
 const koniecSwiata = document.querySelector("#dron");
+let maciek = 0;
 
 aktywator.addEventListener('click', () => {
     //tutaj sie zaczyna prawdziwy męski kod a nie jakies kłeryselektory i strzałki
-    while (1 == 1) {
+    while (maciek < 15000) {
         koniecSwiata.appendChild(document.createElement("p"));
         koniecSwiata.appendChild(document.createTextNode("DUDU REALISTA"));
+        maciek++;
     }
+});
+destruktator.addEventListener('click', () => {
+    document.body.innerHTML = "no dobra"
 });
